c2ba1a27bd6122ee7dc4e10a34b261d68e34cfd7
Jabrilthecoder <94321684+Jabrilthecoder@users.noreply.github.com>
2022-04-13 21:09:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,511 @@
+let age = 12;
+age = 13;
+
+const birthyear = 2009;
+
+const job = coder;
+//day 2 december 14 2021
+console.log(job);
+console.log(birthyear);
+console.log(age)
+const ageMustafa = now - 2023;
+console.log(age, ageMustafa);
+const firstName = 'Jabril';
+const lastName ='Abdilai';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; 
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+// video two day2 december 14 2021
+console.log (age > ageMustafa);
+console.log(ageMustafa >=8);
+
+const isFullAge = age
+
+const now = 2021;
+
+const agejabril = now - 2009;
+const ageMustafa = now - 2013;
+
+console.log(now - 2009 > now - 2013);
+
+//day2 video 3 december 14 2021
+
+let x, y; 
+x= y = 25-10-5;
+console.log(x,y);
+const averageAge = (agejabril + ageMustafa) /2
+console.log(agejabril, ageMustafa, averageAge);
+
+const massJabril = 105;
+const heightJabril = 62.5;
+const massMustafa = 72;
+const heightMustafa = 55.5;
+
+
+
+let firstName ="jabril";
+
+console.log(firstName);
+console.log(firstName); 
+console.log(firstName);
+
+let thefamily="jabril,jazmine,mustafa,makial,noor, hamdi";
+
+console.log(thefamily);
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 12);
+console.log(typeof 'jabril');
+let age = 12 
+age = 12 
+console.log(age)
+// day 3 work december 15 2021 and day 4 december 18 2021
+const noorHeight = 73;
+const noorWeight = "unkown";
+
+const HamdiHeight = 71;
+const HamdiWeight = "N/A";
+
+const jazmineHeight = 63.5;
+const jazmineWeight = 108.5;
+
+console.log(noorWeight < HamdiWeight);
+console.log(noorHeight > HamdiHeight);
+
+console.log(jazmineHeight > heightJabril);
+console.log(jazmineWeight > massJabril);
+
+const makialHeight = 20;
+const makialWeight = 18;
+
+console.log (massMustafa > makialWeight);
+console.log (heightMustafa > makialHeight);
+
+//day 4 work december 19 2021
+
+const birthYEAR = 2009;
+
+let century; if (birthYear <=2000) {
+century = 20;
+} else { century =21;
+} console.log (century);
+
+
+// day 4 work december 19 2021 
+
+'use strict';
+const BMIjazmine = jazmineWeight / jazmineHeight ** 2;
+const BMIjabril = massJabril / heightJabril ** 2;
+console.log(BMIjazmine, BMIjabril);
+
+IF (BMIjabril > BMIjazmine) 
+ { console.log("Jabril's bmi is higher than jazmines") }
+
+  console.log("jazmines BMI is higher than jazmine")
+
+  // day 5 december 20 2021 
+
+
+  const inputYear = '2009';
+  console.log(number (inputYear), inputYear);
+  console.log(number(inputYear)+ 16);
+
+  console(n)
+
+
+  
+
+  const jazmine = "your feet are so long it looks like dinosaurs foot ";
+
+  console.log(jazmine);
+
+  console.log(number("javrul"));
+    console.log(string(12),12);
+
+    console.log('i am' + 12 + ' years old ');
+    console.log('12' + '4' - 4);
+
+    console.log(boolean(10));
+    console.log(boolean(null));
+    console.log(boolean(8));
+    console.log(boolean(12));
+
+const money = 100000;
+    if (money) {console.log("you need a job fatass;)");} else {console.log('are you ok you just got your rich ass scammed');}
+    
+    let height = 5.2;
+    if (height) {console.log('damn!your short got to sleep'); }else {console.log('how tall are you');}
+
+    const favorite  = neegae;
+
+    if (favorite === 12) { console.log('cool! your 12 me too');
+  } else if (favorite=8) {console.log('ew 8 are you 8 and an addict to fortnite dumb smh i am an addic of mm2'); } else {console.log('favortie=im very cool');}
+
+//day 6 decemeber 22 2021 
+
+const hasDriverLiscence = false;
+const imBored = true;
+
+console.log(hasDriverLiscence && imBored);
+console.log(hasDriverL || hasGood);
+console.log(!hasDriverLiscence);
+
+const isTired = True; 
+
+
+
+if (hasDriversLiscense && imBored && imTired); {
+
+console.log('jabril is not able to drive')
+}
+
+//booleans today video 23 stoping point n      
+
+const scoreDolphins =( 97 + 112 + 80 ) / 3;
+const scoreKoalas = (109 +95 + 50 ) /3; 
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {console.log('dolphins win the trophy');} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {console.log('koalas win the trophy');} else if (scoreKoalas === scoreDolphins && scoreKoalas >= 100) { console.log('koalas win the trophy');}else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {console.log('both win tie!')} else {console.log('No one wins the trophy');
+}
+//day 7 switch statements dec 25 2021
+
+const day = 'monday';
+
+switch(day) {
+  case 'monday' : // day ==='monday'
+  console.log('plane yt video');
+  console.log ('yt video');
+  console.log('do my work');
+  console.log('school');
+
+    case 'friday' : 
+    
+    console.log('coding,kumon,reading');
+    console.log('play on phone');
+    console.log('clean kitchen for 2 hours');
+    console.log('play');
+    console.log ('go to bed');
+
+  
+}
+// video 27
+"3 + 4"
+"1991"
+"true && false && !false <- statement"
+"ex of code  if (23 > 10) {const str = '23 is bigger '; } strings are expresions "
+"console.log(im $(2037 - 1991) years old ${me'}"
+
+// video 28
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+let drink2;
+if (age >=21) {drink2 = 'wine';
+} else {drink2 ='water';} console.log(drink2);
+console.log(`i like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+// day 7 video 29 - 33
+const credits = +300000;
+// 😁👍 nice 
+const executionTax = bill <= 300000 && bill >= 30 ? bill * 0.15 :
+bill * 0.2; 
+crossOriginIsolated.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+// 😵 good luck
+
+// tip ammount  ^
+            // |  |
+
+// video 33
+
+function logger() {
+  console.log('My name is jonas')
+
+}
+
+
+logger();
+logger();
+logger();
+
+number(12);
+
+// video 34 
+
+function calcAge1(birthYear) {return 2037 - y;}
+const age = calcAge1(y);
+// function expression
+const age2 = calcAge2(1991);
+
+console.log(age1,age2);
+
+// video 35
+
+
+const calcAge3 = function(y) {
+  return 2009 - birthYear;
+}
+
+birthYear => 2009 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirment = birthYear => { const age = 2009 - birthYear 
+const retirement = 65 - age;
+return `${firstName} retires in ${ retirement}
+years`;
+
+}
+console.log(yearsUntilRetirment(1991,Jonas));
+console.log(yearsUntilRetirment(2009,Jabril));
+
+//video 36
+
+
+function cutFruitPieces(fruit) 
+{
+  return fruit * 4;
+}
+
+function fruitProcessor(apples,oranges) 
+{
+  const juice = `juice with ${applePieces} piece of apple and ${orangePieces}
+  pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+//day37
+
+const calcAverage = (345);
+let scoreDolphins =( 97 + 112 + 80 ) / 3;
+let scoreKoalas = (109 +95 + 50 ) /3; 
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {console.log('dolphins win the trophy');} 
+else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {console.log('koalas win the trophy');} 
+else if (scoreKoalas === scoreDolphins && scoreKoalas >= 100) { console.log('koalas win the trophy');}
+else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {console.log('both win tie!')}
+ else {console.log('No one wins the trophy');
+} console.log('no team wins');
+
+checkWinner (scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+//test 2
+scoreDolphins = calcAverage(85,54,41);
+scoreKoalas - calcAverage(23,34,27);
+console.log(scoreDolphins,scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+//video 39
+
+const friend1 = 'jojo'
+const friend2 = 'jabril'
+const friend = ['shit','jojo','fuart'];
+console.log(friends); 
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friend[0]);
+console.log(friend[2]);
+
+console.log(friends.length);
+console.log(friends[friends. length - 1]);
+friend[2]
+= 'jay';
+ const firstName = 'jonas'; 
+ const jonas = [firstName, 'jon', 2037-1991,
+'teacher', friends]
+console.log
+
+const calcAge = function (birthyear){return 2037 - birthyear; }
+const years = [1990,1967,2009,2010,2018];
+
+  
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1,age2,age3); 
+
+const calcTip = function (bill) { return bill >= 50 && bill <= 300 ? bill * 0.15 : bill *0.2;}
+
+const bills = [125,555,44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills)];
+const totals = [bills[0]] + tips[0] + [bills[1]] + [tips[0] , [bills[2] ]+ tips[2]];
+console.log(bills , tips , totals);
+
+//day 20 
+
+const jonasArray = [ 'jonas ', 'jabril',
+2009, 1991,
+'teacher','student']
+friend[none];
+ 
+
+const jabril = ['jabril',
+2009]
+
+const mustafa = {fullName : 'mustafa  abdilahi',
+ mass: 49 };
+
+ const jazmine = {fullName : 'jazmine abdilahi',
+ mass: 89 ,};
+
+
+ console.log('Lifting weights repetition 1');
+ console.log('Lifting weights repetition 2');
+ console.log('Lifting weights repetition 3');
+ console.log('Lifting weights repetition 4');
+ console.log('Lifting weights repetition 5');
+ console.log('Lifting weights repetition 6');
+ console.log('Lifting weights repetition 7');
+ console.log('Lifting weights repetition 8');
+ console.log('Lifting weights repetition 9');
+ console.log('Lifting weights repetition 10');
+
+ for (let rep = 1; rep <= 10; rep++) { console.log(`Lifting weights repetition ${rep}`);}
+
+ const jonasArray = [ 'jonas ', 
+ 'jabril',
+2009,
+ 1991,
+'teacher',
+'student']
+friend[
+  none
+];
+
+for(let i =0; i< 5; i++) {
+  console.log(jonas[i])
+}
+console.log(jonas[i], typeof jonas[i]);type.push(typeof jonas[i]);
+
+console.log(types);
+
+const years = [1991,2007,1969,2009];
+const ages = [];
+
+for (let i = 0; i< years.length; i++) {ages.push(2009 - years[i]);
+}
+
+console.log(ages);
+
+console.log('--only strings---')
+for (let i =0; i < jonas.length;
+  i++ )
+{if(typeof jonas[i], typeof jonas[i]);}
+ 
+
+const jonasArray = [ 'jonas ', 
+'jabril',
+2009,
+1991,
+'teacher',
+'student']
+friend[
+ none
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {console.log(i,jonas[i]);
+}
+
+for (let rep = 1; rep <= 10; rep++) 
+{ console.log
+  (`Lifting weights repetition ${rep}`)
+  ;}
+
+
+  for
+  (let exercise =1; exercise <4; exercise++)
+   {
+    console.log
+    (`---------starting exercise${exercise}`)
+}
+
+let dice = Math.trunc(math.random()*6)+1;
+if (dice === 6){
+   console.log('Loop is about to end... ');
+}
+const bill = 100;
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15:
+  bill * 0.2;
+  
+  y
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100,];}
+
+for (let i = 1; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  totals.push(tip + bills[i]);
+  
+  }
+  
+  console. log(bills, tips, totals);
+  
+  const calcAverage = function (arr) { console. log(calcAverage([2, 3, 7]));
+    console. log(calcAverage(totals) ) ;
+    console. log(calcAverage(tips) ) ;
+  } 
+
+  //feburary 5 1:22 video 51,59(most of it was reading and watching)
+  // video 54
+
+  'use strict';
+
+  const x = '23';
+  if (x=== 23) console.log(23);
+  const calcAge  = birthYear => 2037 - birthYear;
+  console.log();
+// video 70-72
+  const calcTempAmplitude = function (temps) {let max = temps[0]
+    let min = temps[0] }
+    for(let i = 0; i < temps.length; i++) {const curTemp = temps[i]
+    if (typeof curTemp > max) max = curTemp;
+    if (typeof curTemp > min) min = curTemp;
+    console.log(max,min);
+    return max-min; }
+    const AmplitudeNEw = calcTempAmplitude(temperatures);
+    console.log(AmplitudeNEw);
+    const array1 = ['a','b','c'];
+    const array2 = ['d','e','f'];
+    const array3 = array1.concat(array2);
+    //Apr 13 2022
+//notes 91
+// notes 92
+
+//93
+'use strict';
+
+function calcAge(birhtYear) {const age = 2037 - birthYear;
+console.log(firstName);
+return age;}
+const firstName = 'Jonas';
+calcAge(1991);
+//notes 94
+
+//hosting video 95
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+console.log(me);
+
+function addDecl(a,b) {
+  return a + b;
+
+}
+const addExpr = function(a,b) {
+   return a+b;
+  };
+const addArrow = (a, b)=> a + b;
+ //96 notes
